246ac43cef3e8a9e913a923af7f97d2a8c6ebf62
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-09-15 13:58:01 +0300

    filling admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+@admin.register(Category)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name', 'description',)
+
+@admin.register(Product)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
