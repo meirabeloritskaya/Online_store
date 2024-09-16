42af3ebbba31199c1e1f775de0a8fd52dc4330a5
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-09-16 07:48:57 +0300

    fix models.py, admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from .models import Category, Product
 
 @admin.register(Category)
-class AuthorAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
     search_fields = ('name', 'description',)
 
 @admin.register(Product)
-class BookAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
--- catalog/models.py
+++ catalog/models.py
@@ -21,8 +21,8 @@ class Product(models.Model):
     image = models.ImageField(verbose_name='Изображение', upload_to='images/', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', related_name='products')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
-    created_at = models.DateField(verbose_name='Дата создания')
-    updated_at = models.DateField(verbose_name='Дата обновления')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
     def __str__(self):
         return self.name
