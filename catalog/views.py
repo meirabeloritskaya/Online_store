81bb29750c68a93da10d93042157f5528bbc0cdb
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-09-18 00:12:05 +0300

    add cards in home.html

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, "home.html", context)
 
 
 def contacts(request):
