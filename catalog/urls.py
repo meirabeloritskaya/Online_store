e18bb30a37059473f0a34fd79c6687dfd9fb2af5
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-09-17 23:23:59 +0300

    create controles and shablones products_list/product_detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
+from catalog import views
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path("", home, name="home"),
     path("contacts/", contacts, name="contacts"),
+    path('products/', views.products_list, name='products_list'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 def home(request):
     return render(request, "home.html")
@@ -7,3 +7,13 @@ def home(request):
 
 def contacts(request):
     return render(request, "contacts.html")
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/products_list.html', context)
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
