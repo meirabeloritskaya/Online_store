a9ded7f299d57e5a8756a45c9ba4f3b2e678905a
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-09-27 17:38:47 +0300

    created files.html in blog

--- catalog/views.py
+++ catalog/views.py
@@ -23,4 +23,4 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
-    slug_field = 'id'  # Используйте, если ваш URL использует slug, иначе можете оставить
+    slug_field = 'id'
