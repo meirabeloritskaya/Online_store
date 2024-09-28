7c56ec27cde1db5ffa1bab500c03d239d48a56b1
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-09-29 00:01:51 +0300

    change field is_published by published

--- blog/views.py
+++ blog/views.py
@@ -8,25 +8,35 @@ class BlogPostListView(ListView):
     template_name = 'blog/blogpost_list.html'
     context_object_name = 'blogposts'
 
+    def get_queryset(self):
+        return BlogPost.objects.filter(published=True)
+
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
     template_name = 'blog/blogpost_detail.html'
-    context_object_name = 'blogpost'
+
+    def get_object(self):
+        post = super().get_object()
+        post.views_count += 1
+        post.save(update_fields=['views_count'])
+        return post
 
 
 class BlogPostCreateView(CreateView):
     model = BlogPost
     template_name = 'blog/blogpost_form.html'
-    fields = ['title', 'content', 'preview_image', 'is_published']
+    fields = ['title', 'content', 'preview_image', 'published']
     success_url = reverse_lazy('blog:blogpost_list')
 
 
 class BlogPostUpdateView(UpdateView):
     model = BlogPost
     template_name = 'blog/blogpost_form.html'
-    fields = ['title', 'content', 'preview_image', 'is_published']
-    success_url = reverse_lazy('blog:blogpost_list')
+    fields = ['title', 'content', 'preview_image', 'published']
+
+    def get_success_url(self):
+        return reverse_lazy('blog:blogpost_detail', kwargs={'pk': self.object.pk})
 
 
 class BlogPostDeleteView(DeleteView):
